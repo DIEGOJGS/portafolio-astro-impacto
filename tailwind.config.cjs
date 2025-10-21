3d7f862f270b9e698abdfd37704b39bcc9609d12
DIEGOJGS <diegojgsald@gmail.com>
2025-10-21 12:43:22 -0500

    feat(design): Añadir fuente 'Inter' y componente 'Container'

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,13 @@ export default {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
   ],
-  theme: {
-    extend: {},
-  },
+    theme: {
+    extend: {
+        // Añadimos 'Inter' como fuente principal
+        fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        },
+    },
+    },
   plugins: [],
 }
