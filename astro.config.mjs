3423237ba79d8de5455ec8426a736694608c81f8
DIEGOJGS <diegojgsald@gmail.com>
2025-10-21 12:40:25 -0500

    fix(deps): Arreglar conflicto de versiones de Tailwind v3/v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
-import react from '@astrojs/react';
-
-import tailwindcss from '@tailwindcss/vite';
+// Esta es la importación correcta
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [
+    react(), 
+    tailwind() // Aquí se activa la integración
+  ]
 });
